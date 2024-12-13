b1d2f4752b92815264d8b807193be41ba2899143
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2024-12-14 01:37:21 +0300

    .

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -45,131 +45,83 @@ const Header = () => {
   const isActive = (path) => router.pathname === path;
 
   return (
-    <>
-      <nav
-        className={`navbar navbar-expand-md navbar-dark ${styles.navColor} fixed-top`}
-      >
-        <div className="container">
-          <Link className={`navbar-brand ${styles.logo}`} href="/">
-            nextWatch
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <Link
-                  className={`nav-link ${styles.link}`}
-                  aria-current="page"
-                  href="/"
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className={`nav-link ${styles.link}`}
-                  aria-current="page"
-                  href="/movie"
-                >
-                  Movies
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className={`nav-link ${styles.link}`} href="/tv">
-                  Tv Shows
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className={`nav-link ${styles.link}`} href="/trends">
-                  Trends
-                </Link>
-              </li>
-              {isMobile ? (
-                <div className={`py-2 ${styles.link}`} onClick={openNclose}>
-                  Search
-                </div>
-              ) : (
-                <div className={styles.icons}>
-                  <FaSearch className={styles.icon} onClick={openNclose} />
-                </div>
-              )}
-            </ul>
-          </div>
-        </div>
-
-        {isOpen && (
-          <div className={styles.searchContainer}>
-            <div className={styles.searchWrapper}>
-              <input
-                type="text"
-                placeholder="Search Movies"
-                className={styles.searchInput}
-                value={query}
-                onChange={HandleChange}
-                onKeyUp={searchQueryHandler}
-              />
-              <VscChromeClose
-                className={styles.closeIcon}
-                onClick={openNclose}
-              />
-            </div>
-          </div>
-        )}
-      </nav>
-
-      {/* <div className={styles.navColor}>
-        <div className={styles.navbarContainer}>
-          <div className={styles.logo}>
-            <Link href="/">nextWatch</Link>
-          </div>
-          <div className={styles.linkContainer}>
-            <div className={styles.links}>
-              <Link href="/movie" className={styles.link}>
+    <nav
+      className={`navbar navbar-expand-md navbar-dark ${styles.navColor} fixed-top`}
+    >
+      <div className="container">
+        <Link className={`navbar-brand ${styles.logo}`} href="/">
+          nextWatch
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${styles.link}`}
+                aria-current="page"
+                href="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${styles.link}`}
+                aria-current="page"
+                href="/movie"
+              >
                 Movies
               </Link>
-              <Link href="/tv" className={styles.link}>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link ${styles.link}`} href="/tv">
                 Tv Shows
               </Link>
-              <Link href="/trends" className={styles.link}>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link ${styles.link}`} href="/trends">
                 Trends
               </Link>
-            </div>
-            <div className={styles.icons}>
-              <FaSearch className={styles.icon} onClick={openNclose} />
-            </div>
+            </li>
+            {isMobile ? (
+              <div className={`py-2 ${styles.link}`} onClick={openNclose}>
+                Search
+              </div>
+            ) : (
+              <div className={styles.icons}>
+                <FaSearch className={styles.icon} onClick={openNclose} />
+              </div>
+            )}
+          </ul>
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className={styles.searchContainer}>
+          <div className={styles.searchWrapper}>
+            <input
+              type="text"
+              placeholder="Search Movies"
+              className={styles.searchInput}
+              value={query}
+              onChange={HandleChange}
+              onKeyUp={searchQueryHandler}
+            />
+            <VscChromeClose className={styles.closeIcon} onClick={openNclose} />
           </div>
         </div>
-
-        {isOpen && (
-          <div className={styles.searchContainer}>
-            <div className={styles.searchWrapper}>
-              <input
-                type="text"
-                placeholder="Search Movies"
-                className={styles.searchInput}
-                value={query}
-                onChange={HandleChange}
-                onKeyUp={searchQueryHandler}
-              />
-              <VscChromeClose
-                className={styles.closeIcon}
-                onClick={openNclose}
-              />
-            </div>
-          </div>
-        )}
-      </div> */}
-    </>
+      )}
+    </nav>
   );
 };
 
