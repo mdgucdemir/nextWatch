f55ed2f8895a30951d4abd6b1eb9d2cd5494bdc6
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2024-12-15 22:02:16 +0300

    .

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Header from "@/components/header/Header";
 import "./globals.css";
 import { Suspense } from "react";
+import Head from "next/head";
 
 export const metadata = {
   title: "nextWatch",
@@ -10,6 +11,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+          integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+          integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body>
         <Suspense>
           <Header />
--- app/page.js
+++ app/page.js
@@ -1,27 +1,23 @@
 "use client";
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import MediaBanners from "@/components/mediaBanners/MediaBanners";
 import Banner from "@/components/banner/Banner";
 import dynamic from "next/dynamic";
-import Head from "next/head";
-// import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-const Bootstrap = dynamic(
-  () => import("bootstrap/dist/js/bootstrap.bundle.min.js"),
-  { ssr: false }
-);
+// const Bootstrap = dynamic(
+//   () => import("bootstrap/dist/js/bootstrap.bundle.min.js"),
+//   { ssr: false }
+// );
 
 export default function Home() {
-  useEffect(() => {
-    Bootstrap();
-  }, []);
+  // useEffect(() => {
+  //   Bootstrap();
+  // }, []);
   return (
     <div>
-      <Head>
-        <Banner mediaType="trending/movie/day?" type="banner" />
-        <MediaBanners />
-      </Head>
+      <Banner mediaType="trending/movie/day?" type="banner" />
+      <MediaBanners />
     </div>
   );
 }
