f4a6440fa672e6ca3226eebaf9051554504de437
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2024-12-15 22:16:08 +0300

    .

--- app/page.js
+++ app/page.js
@@ -1,19 +1,9 @@
 "use client";
 
-// import { useEffect } from "react";
 import MediaBanners from "@/components/mediaBanners/MediaBanners";
 import Banner from "@/components/banner/Banner";
-import dynamic from "next/dynamic";
-
-// const Bootstrap = dynamic(
-//   () => import("bootstrap/dist/js/bootstrap.bundle.min.js"),
-//   { ssr: false }
-// );
 
 export default function Home() {
-  // useEffect(() => {
-  //   Bootstrap();
-  // }, []);
   return (
     <div>
       <Banner mediaType="trending/movie/day?" type="banner" />
